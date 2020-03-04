58abfccb0deeeca28ec87918d3a107569cc8d460
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2020-03-04 11:38:36 +0100

    redux - action - basic structure

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 // STORE - GLOBALIZED STATE
 // holds all data for our application
 
-// ACTION - ex. 'Increment'
+// ACTION - ex. 'Increment' - os only a describtion of a process
+// - an action is afunction that returnes an object
+const incremenet = () => {
+  return {};
+};
 
 // REDUCER - describes how our Actions transform state into the next state
 
 // DISPATCH - executes the action
-ReactDOM.render(<App />, document.getElementById('root'))
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
