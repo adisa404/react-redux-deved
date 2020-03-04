ec1a714be37d0c023314032b5f364c517a8233e0
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2020-03-04 11:25:11 +0100

    redux - prep

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// STORE - GLOBALIZED STATE
+// holds all data for our application
+
+// ACTION - ex. 'Increment'
+
+// REDUCER - describes how our Actions transform state into the next state
+
+// DISPATCH - executes the action
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
