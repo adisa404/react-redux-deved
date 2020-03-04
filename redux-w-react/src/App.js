54856284dd1d7b612b109c2fb0fbf79f3ed3ee31
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2020-03-04 15:21:44 +0100

    project - redux with react - use global state with useSelector.

--- redux-w-react/src/App.js
+++ redux-w-react/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const counter = useSelector(state => state.counterReducer);
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <h1>Counter from state {counter}</h1>
     </div>
   );
 }
