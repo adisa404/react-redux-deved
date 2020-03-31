5543b753da74b1e9d2b741d36fd5b7a554dbdacf
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2020-03-31 12:52:08 +0200

    redux ex 2.3 - add new action

--- exercise-files-linkedin-tekumalla/my-work/todomvc-02-03-start/src/components/BookItem.js
+++ exercise-files-linkedin-tekumalla/my-work/todomvc-02-03-start/src/components/BookItem.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import classnames from 'classnames'
-import {BookConsumer} from '../context/BookContext';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { BookConsumer } from '../context/BookContext';
 
 export default class BookItem extends Component {
   static propTypes = {
@@ -9,54 +9,63 @@ export default class BookItem extends Component {
     editBook: PropTypes.func.isRequired,
     deleteBook: PropTypes.func.isRequired,
     completeBook: PropTypes.func.isRequired
-  }
+  };
   constructor(props) {
     super(props);
     this.statusChange = this.statusChange.bind(this);
     this.state = {
-      completed: this.props.book.completed,
-    }
+      completed: this.props.book.completed
+    };
   }
 
   statusChange() {
-    const {book, updateBookStatus} = this.props;
-    const status =  !book.status || book.status === 'unread' ? 'completed' : 'unread'
-    this.setState({completed: status})
+    const { book, updateBookStatus } = this.props;
+    const status =
+      !book.status || book.status === 'unread' ? 'completed' : 'unread';
+    this.setState({ completed: status });
     updateBookStatus(book.id, status);
   }
-  
+
   render() {
-    const { book, updateBookStatus} = this.props
-    const {volumeInfo} = book;
-    const bookCompleted = book.status === 'completed';
-    
-     const element = (
-       <BookConsumer>
-         {({showAuthors}) => (
-        <div className="view">
-          <input className="toggle"
-                 type="checkbox"
-                 checked={bookCompleted}
-                 onChange={() => this.statusChange()} />
-          <label>
-            {volumeInfo.title}
-          </label>
-          {showAuthors ? 
-          <p style={{fontSize: '12px', textAlign: 'right', marginRight: '10px'}}>
-            Author: {volumeInfo.authors[0]}
-          </p> : null
-          }
-        </div>
-         )}
-        </BookConsumer>
-      )
+    const { book, updateBookStatus } = this.props;
+    const { volumeInfo } = book;
+    const bookCompleted = book.completed;
+
+    const element = (
+      <BookConsumer>
+        {({ showAuthors }) => (
+          <div className='view'>
+            <input
+              className='toggle'
+              type='checkbox'
+              checked={bookCompleted}
+              onChange={() => this.statusChange()}
+            />
+            <label>{volumeInfo.title}</label>
+            {showAuthors ? (
+              <p
+                style={{
+                  fontSize: '12px',
+                  textAlign: 'right',
+                  marginRight: '10px'
+                }}
+              >
+                Author: {volumeInfo.authors[0]}
+              </p>
+            ) : null}
+          </div>
+        )}
+      </BookConsumer>
+    );
 
     return (
-      <li className={classnames({
-        completed: bookCompleted,
-      })}>
+      <li
+        className={classnames({
+          completed: bookCompleted
+        })}
+      >
         {element}
       </li>
-    )
+    );
   }
 }
--- exercise-files-linkedin-tekumalla/my-work/todomvc-02-03-start/src/reducers/books.js
+++ exercise-files-linkedin-tekumalla/my-work/todomvc-02-03-start/src/reducers/books.js
@@ -4,20 +4,29 @@ import {
   EDIT_TODO,
   COMPLETE_TODO,
   COMPLETE_ALL_TODOS,
-  CLEAR_COMPLETED
-} from '../constants/ActionTypes'
+  CLEAR_COMPLETED,
+  BOOK_STATUS_UPDATED
+} from '../constants/ActionTypes';
 
 import classicBooks from '../constants/books';
 
 const initialState = {
   books: classicBooks,
   completedBooks: 0
-}
-   
+};
 
 export default function todos(state = initialState, action) {
   switch (action.type) {
+    case BOOK_STATUS_UPDATED:
+      return {
+        ...state,
+        books: state.books.map(book => {
+          return book.id === action.id
+            ? { ...book, completed: !book.completed }
+            : book;
+        })
+      };
     default:
-      return state
+      return state;
   }
 }
