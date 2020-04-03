8110425f6069d22bacc2404e2f0c7d687be50351
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2020-04-03 21:40:29 +0200

    redux thunk shaun wassel - 3.8 -  add redux-persist.

--- redux-thunk-linkedin-shaun-wassel/ch02_08/src/index.js
+++ redux-thunk-linkedin-shaun-wassel/ch02_08/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/es/integration/react';
 import { configureStore } from './store';
 import App from './App.js';
 
+const store = configureStore();
+const persistor = persistStore(store);
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
